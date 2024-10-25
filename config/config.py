3744a3a9e6c296a965d6f0ace10432b37138079d
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-10-25 10:32:05 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -96,7 +96,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MOON-CODES/SUNLIGHT",
+    "https://envs.sh/pi0.jpg",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
