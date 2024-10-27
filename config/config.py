085e979d06b4d393fa184ce2e1dc3bc7d4cc918f
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-10-27 23:49:00 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -93,12 +93,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 
-TOKEN_ALLOW = os.getenv("{
-    "access_token": "ya29.a0AeDClZATqpnJLcdG_2NWC0DJqnYKlT1_leu_mjfpxbKaY43nHepOyhTcpa6HKhK2GD5YvkssYWtAAEyfsnnJtwC1jpSsPezc6qcMkJPQN0WlSppCmVtHNwlTYx3RB9rG8ku6GvlfQY8b76kky-ZXbOF8E0m6QvPc2qmhFstYanXa_emrow1BaCgYKAYgSARMSFQHGX2MiabHLX498l7ehNx8F5KVilA0187",
-    "expires": 1730137904.620908,
-    "refresh_token": "1//06Fm4WDyy7DwgCgYIARAAGAYSNwF-L9Ir57UadAFPtK2uoTLF5940Q1AVhjcb9MVojd0wIfXkHUv3BBuxkaQpfKikZTpaYs2Zi0U",
-    "token_type": "Bearer"
-}", "False")
+TOKEN_ALLOW = os.getenv("TOKEN_ALLOW", "False")
 
 
 # For customized or modified Repository
